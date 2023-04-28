a9c9b4877a83189b45741c5a20da875a97934798
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-04-28 11:42:44 -0300

    updating UI

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUi()
     }
 
+    func updateUi() {
+        storyLabel.text = storyBrain.story[0].title
+        choice1Button.setTitle(storyBrain.story[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.story[0].choice2, for: .normal)
+    }
 
 }
 
